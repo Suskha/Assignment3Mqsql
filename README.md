2993f811a95b1b43d74bb870ee33c445a01eacbf
SusKha <sushil.khadka@fusemachines.com>
2017-12-18 10:19:58 +0545

    student.sql file is added

---
+++ README.md
@@ -0,0 +1,13 @@
+# How to run the project
+
+***
+
+* First of all you hava to add the data(student.sql) to you database.
+	- which creates the database (student) and store the data in the table name(student). 
+	Export the student.sql file without selecting any database.
+
+* run the program and give your database username and password.
+
+*** 
+
+### The jar file is present in jar folder.
--- src/com/database/DatabaseConnect.java
+++ src/com/database/DatabaseConnect.java
@@ -1,17 +1,29 @@
 package com.database;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class DatabaseConnect {
 
+	private Scanner input = new Scanner(System.in);
+	private String userName ;
+	private String password;
+	
 	public void DatabaseConn() {
 
 		WithIndex withIndex = new WithIndex();
 		WithoutIndex withoutIndex = new WithoutIndex();
 		
+		System.out.print("Enter the username of your database : ");
+		userName = input.nextLine();
+		System.out.print("Enter the password of your database : ");
+		password = input.nextLine();
+		System.out.println();
+		
+		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/student", "root", "");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/student", userName, password);
 			withIndex.getTimeWithoutIndex(con);
 			withoutIndex.getTimeWithIndex(con);
 			con.close();
